7b80050f5d19278a24bf2a3ce862af2622ece245
Vasanth <pvasanth10102001@gmail.com>
2025-04-27 21:53:43 +0530

    UI Update

--- src/components/CoreConcept.jsx
+++ src/components/CoreConcept.jsx
@@ -4,45 +4,46 @@ import TabButton from "./TabButton";
 import { exampleData } from "../data.js";
 
 export default function CoreConcept({ concept }) {
-    const [content, setContent] = useState(null);
+  const [content, setContent] = useState(null);
+  const Tabs = ["Component", "Props", "State", "JSX"];
 
-    const Tabs = ["Component", "Props", "State", "JSX"];
+  return (
+    <>
+      <section className="core-concept">
+        {concept.map((data, index) => (
+          <div key={index} className="concept">
+            <img src={data.image} alt={data.name} className="image" />
+            <h1>{data.name}</h1>
+            <p>{data.description}</p>
+          </div>
+        ))}
+      </section>
 
-    return (
-        <>
-            <section className="core-concept">
-                {concept.map((data, index) => (
-                    <div key={index} className="concept">
-                        <img src={data.image} alt={data.name} className="image" />
-                        <h1>{data.name}</h1>
-                        <p>{data.description}</p>
-                    </div>
-                ))}
-            </section>
+      <section className="second-Core-div">
+        <h2 className="h2Header">Examples :</h2>
+        <div>
+          {Tabs.map((tab, index) => (
+            <TabButton
+              key={index}
+              onTrigger={() => setContent(tab.toLowerCase())}
+            >
+              {tab}
+            </TabButton>
+          ))}
 
-            <section className="second-Core-div">
-                <h2 className="h2Header">Examples :</h2>
-                <div>
-                    {Tabs.map((tab, index) => (
-                        <TabButton key={index} onTrigger={() => setContent(tab.toLocaleLowerCase())}>
-                            {tab}
-                        </TabButton>
-                    ))}
-
-                    {content ? (
-                        <div className="example-content">
-                            <h3>{exampleData[content].name}</h3>
-                            <p>{exampleData[content].description}</p>
-                            <p>
-                                <p>{exampleData[content].code}</p>
-                            </p>
-                        </div>
-                    ) : (
-                        <p className="content">Please click on the tab to see the content</p>
-                    )}
-                </div>
-
-            </section>
-        </>
-    );
+          {content ? (
+            <div className="example-content">
+              <h3>{exampleData[content].name}</h3>
+              <p>{exampleData[content].description}</p>
+              <pre>
+                <code>{exampleData[content].code}</code>
+              </pre>
+            </div>
+          ) : (
+            <p className="content">Please click on a tab to see the content.</p>
+          )}
+        </div>
+      </section>
+    </>
+  );
 }
